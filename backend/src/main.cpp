5a063154917bc50df7699f5c354800854de92f93
Tristan-Phillips <81848746+Tristan-Phillips@users.noreply.github>
2024-10-26 13:04:19 +0200

    Testing loading from .env file

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,4 +1,5 @@
 #include "crow.h"
+#include "config.h"
 #include <pqxx/pqxx>
 #include <string>
 #include <iostream>
@@ -7,11 +8,16 @@ using namespace std;
 
 
 int main () {
+    Config config;
+    std::string hostname = config.get("HOSTNAME");
+    std::string port = config.get("PORT");
+    std::cout << "Backend is running on: " << hostname << ":" << port << std::endl;
+
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")([](){
         return "Welcome to the Timeline API";
     });
 
-    app.port(3000).multithreaded().run();
+    app.port(std::stoi(port)).multithreaded().run();
 }
