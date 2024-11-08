b1a11c4604de9ee9e77cd5dd0a1438501a0f266b
Tristan-Phillips <81848746+Tristan-Phillips@users.noreply.github>
2024-11-08 09:06:13 +0200

    remove logUtils class because it was useless

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -2,7 +2,7 @@
 #include "mockDatabase.h"
 #include "config.h"
 #include "smtp.h"
-#include "loggerUtil.h"
+#include "logger.h"
 
 int main() {
     Logger& logger = Logger::getInstance();
@@ -12,7 +12,7 @@ int main() {
     std::unordered_map<std::string, std::string> env = config.readEnv();
     MockDatabase database(env["PGUSER"], env["PGPASSWORD"], env["PGHOST"], env["PGPORT"], env["PGDATABASE"]);
 
-    getLogger().log("main_init", LogLevel::INFO);
+    logger.log("main_init", LogLevel::INFO);
 
     return 0;
 }
